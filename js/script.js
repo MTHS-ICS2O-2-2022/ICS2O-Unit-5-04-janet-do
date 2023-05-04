ca024bbac097275805997c76547b556bb99929b5
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-04 17:01:50 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -7,21 +7,16 @@
 "use strict"
 
 function calculate() {
-  const ageInput = document.getElementById("age").value
-  const age = parseFloat(ageInput)
+  const age = document.getElementById("age").value
+  document.getElementById("age").innerHTML = userAge
+ const day = document.getElementById("day")
+ document.getElementById("day").innerHTML = day
 
-  if (isNaN(age)) {
-  } else if (age >= 17) {
-    document.getElementById("age-restriction").innerHTML =
-      "<p>You can see an R rated movie alone.</p>"
-  } else if (age >= 13) {
-    document.getElementById("age-restriction").innerHTML =
-      "<p>You can see a PG-13 movie alone.</p>"
-  } else if (age >= 6) {
-    document.getElementById("age-restriction").innerHTML =
-      "<p>You can see a G or PG movie alone.</p>"
-  } else {
-    document.getElementById("age-restriction").innerHTML =
-      "<p>You're too young to see anything.</p>"
-  }
+ if (day == "tuesday" || day == "thursday" || (age > "12" && age < "21")) {
+  document.getElementById("price").innerHTML =
+    "You are eligible for discounted pricing."
+ } else {
+  document.getElementById("price").innerHTML = 
+  "You are not eligible for discounted pricing"
+ }
 }
